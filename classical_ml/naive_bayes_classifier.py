a05b50d9e025fc8db1256af5fd3a2aba8491c716
SimondeReuver <dereuversimon@gmail.com>
2025-02-24 19:40:25 +0100

    Initial methods needed for naive bayes and requirements file added

---
+++ classical_ml/naive_bayes_classifier.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from math import e, pi, sqrt
+
+class NaiveBayesClassifier:
+    """Implements Naïve Bayes classification with Gaussian probability density."""
+
+    def __init__(self):
+        self.class_statistics = {}
+
+    def calculate_feature_statistics(self, feature_data: np.ndarray, total_samples: int):
+        
+        return mean, std_dev, prior
+
+    def fit(self, train_features: np.ndarray, train_labels: np.ndarray):
+
+    def gaussian_probability_density_function(self, x: np.ndarray, means: np.ndarray, stdevs: np.ndarray):
+
+    def predict_probabilities(self, X: np.ndarray):
+        
+    def predict(self, X: np.ndarray):
+        
