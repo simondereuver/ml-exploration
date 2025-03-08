62acc9fc47dcb757dd6207ea1e94d258885119ad
SimondeReuver <dereuversimon@gmail.com>
2025-03-08 19:06:48 +0100

    Some stopping criterias implemented

--- classical_ml/decision_tree.py
+++ classical_ml/decision_tree.py
@@ -2,14 +2,29 @@ import numpy as np
 
 class DecisionTree:
     class Node:
-        def __init__(self):
-            pass
+        def __init__(self, feature=None, threshold=None, l_child=None, r_child=None, dtype=None, value=None):
+            self.feature = feature
+            self.threshold = threshold
+            self.l_child = l_child
+            self.r_child = r_child
+            self.dtype = dtype
+            self.value = value
 
-    def __init__(self):
-        pass
+        def is_leaf_node(self):
+            return self.value is not None
 
-    def fit(self):
-        pass
+    def __init__(self, max_depth=10, min_samples=5, imbalance_threshold=0.90):
+        self.max_depth_val = max_depth
+        self.min_samples = min_samples
+        self.imbalance_threshold = imbalance_threshold
+        self.root = None
+
+    def _grow_tree(self, data: np.ndarray, features: list, depth: int):
+
+        if self._is_homogeneous(data): #basecase
+            return self.Node(value=self.majority_class(data))
+
+        return ...
 
     def predict(self):
         pass
@@ -20,17 +35,24 @@ class DecisionTree:
     def _grow_tree(self):
         pass
 
-    def _majority_class(self):
-        pass
+    def _majority_class(self, data: np.ndarray):
+        labels, counts = np.unique(data[:, -1], return_counts=True)
+        return labels[np.argmax(counts)]
 
-    def _is_homogeneous(self):
-        pass
+    def _is_homogeneous(self, data: np.ndarray):
+        _, cls_counts = np.unique(data[:, -1], return_counts=True)
+        return len(cls_counts) == 1  #true if data only consists of 1 class
+    
+    def _max_depth(self, depth: int):
+        return depth >= self.max_depth_val
 
-    def _max_depth_reached(self):
-        pass
-
-    def _best_split(self):
-        pass
+    def _is_imbalanced(self, data: np.ndarray):
+        _, counts = np.unique(data[:, -1], return_counts=True)
+        purity = max(counts) / np.sum(counts)
+        return purity > self.imbalance_threshold
+    
+    def _small_node_size(self, data: np.ndarray):
+        return len(data) <= self.min_samples
 
     def _impurity_measure_numeric(self):
         pass
