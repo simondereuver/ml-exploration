4c079b111ee7c889c2ef379249e174fa389a86ab
SimondeReuver <dereuversimon@gmail.com>
2025-03-08 18:01:26 +0100

    Initial methods needed, might add or remove later

---
+++ classical_ml/decision_tree.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class DecisionTree:
+    class Node:
+        def __init__(self):
+            pass
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def _predict_sample(self):
+        pass
+
+    def _grow_tree(self):
+        pass
+
+    def _majority_class(self):
+        pass
+
+    def _is_homogeneous(self):
+        pass
+
+    def _max_depth_reached(self):
+        pass
+
+    def _best_split(self):
+        pass
+
+    def _impurity_measure_numeric(self):
+        pass
+
+    def _impurity_measure_categorical(self):
+        pass
+
+    def _gini_index(self):
+        pass
+
+    def _split_data(self):
+        pass
